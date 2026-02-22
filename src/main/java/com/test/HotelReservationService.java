1eef2c61da8463d71eb4544e16c83eff6f2e4693
Kamalpreet Kaur <kamalpreet19@gmail.com>
2026-02-22 16:44:57 +0530

    usecase 4

--- src/main/java/com/test/HotelReservationService.java
+++ src/main/java/com/test/HotelReservationService.java
@@ -12,18 +12,26 @@ public class HotelReservationService {
     }
 
     public String findCheapestHotel(int[] dayTypes) {
-        Hotel cheapest = null;
+        ArrayList<String> cheapestHotels = new ArrayList<>();
         int minCost = Integer.MAX_VALUE;
 
         for (Hotel hotel : hotelList) {
             int cost = hotel.calculateTotalCost(dayTypes);
+
             if (cost < minCost) {
+
                 minCost = cost;
-                cheapest = hotel;
+                cheapestHotels.clear();
+                cheapestHotels.add(hotel.getName());
+            } else if (cost == minCost) {
+
+                cheapestHotels.add(hotel.getName());
             }
         }
 
-        return cheapest.getName() + ", Total Rates: $" + minCost;
+
+        String hotelNames = String.join(" and ", cheapestHotels);
+        return hotelNames + ", Total Rates: $" + minCost;
     }
 
 
--- src/test/java/com/test/HotelReservationServiceTest.java
+++ src/test/java/com/test/HotelReservationServiceTest.java
@@ -6,10 +6,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HotelReservationServiceTest {
+    @Test
+    public void testFindCheapestHotel_SingleOrTie() {
+        HotelReservationService service = new HotelReservationService();
+
+        service.addHotel("Lakewood", 110, 90);
+        service.addHotel("Bridgewood", 110, 90);
+        service.addHotel("Ridgewood", 220, 150);
+
+        int[] days = {1, 1};
+
+        String expected = "Lakewood and Bridgewood, Total Rates: $220";
+        String actual = service.findCheapestHotel(days);
+
+        assertEquals(expected, actual);
+    }
 
     @Test
-    public void testFindCheapestHotel() {
+    public void testFindCheapestHotel_NoTie() {
         HotelReservationService service = new HotelReservationService();
+
+
         service.addHotel("Lakewood", 110, 90);
         service.addHotel("Bridgewood", 150, 50);
         service.addHotel("Ridgewood", 220, 150);
@@ -19,6 +36,6 @@ public class HotelReservationServiceTest {
         String expected = "Lakewood, Total Rates: $220";
         String actual = service.findCheapestHotel(days);
 
-        assertEquals(expected, actual); // This is the test check
+        assertEquals(expected, actual);
     }
 }
